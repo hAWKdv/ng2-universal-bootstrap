91da4f2a134df381fe9041e36e55f6cd47e0c625
hAWKdv <hawk.dvp@gmail.com>
2016-04-02 14:51:36 +0300

    Feat(Boot function): Add window.Worker check

--- src/ng2-universal-bootstrap/ng2-universal-bootstrap.ts
+++ src/ng2-universal-bootstrap/ng2-universal-bootstrap.ts
@@ -1,9 +1,22 @@
 var ng2UniversalBootstrap = ng2UniversalBootstrap || (function () {
-  let boot = (init) => {
-    if (init) {
-      System.import("/node_modules/angular2/bundles/web_worker/ui.dev.js")
+  'use strict';
+  
+  let getWebWorkerConfig = (config) => {
+    if (typeof config.webworker === 'undefined') {
+      return !!window.Worker;
+    }
+    
+    return config.webworker;
+  };
+  
+  let boot = (config) => {
+    config = config || {};
+    config.webworker = getWebWorkerConfig(config);
+        
+    if (config.webworker) {
+      System.import('/node_modules/angular2/bundles/web_worker/ui.dev.js')
         .then(() => {
-          System.import("./dist/ng2-universal-bootstrap/worker/bootstrap")
+          System.import('./dist/ng2-universal-bootstrap/worker/bootstrap')
             .catch(() => { console.log('error'); });            
         });
 
@@ -20,7 +33,7 @@ var ng2UniversalBootstrap = ng2UniversalBootstrap || (function () {
       
       console.log('Workers Off');
     }
-  } 
+  };
   
   return boot;
 }());
