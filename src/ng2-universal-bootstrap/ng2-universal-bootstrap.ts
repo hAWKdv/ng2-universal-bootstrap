3fdd3d60d5991115764ee6f62e8a5b1ba85363a5
hAWKdv <hawk.dvp@gmail.com>
2016-04-02 16:23:15 +0300

    Feat(Boot function): Improve library loading

--- src/ng2-universal-bootstrap/ng2-universal-bootstrap.ts
+++ src/ng2-universal-bootstrap/ng2-universal-bootstrap.ts
@@ -12,27 +12,27 @@ var ng2UniversalBootstrap = ng2UniversalBootstrap || (function () {
   let boot = (config) => {
     config = config || {};
     config.webworker = getWebWorkerConfig(config);
+    
+    let bootstrap: string;
+    let libs: string[];
         
     if (config.webworker) {
-      System.import('/node_modules/angular2/bundles/web_worker/ui.dev.js')
-        .then(() => {
-          System.import('./dist/ng2-universal-bootstrap/worker/bootstrap')
-            .catch(() => { console.log('error'); });            
-        });
-
-      console.log('Workers On');
-    } else {        
-      System.import('/node_modules/angular2/bundles/angular2.dev.js')
-        .then(() => {
-          System.import('/node_modules/rxjs/bundles/Rx.js')
-            .then(() => {
-              System.import('./dist/ng2-universal-bootstrap/main/bootstrap')
-                .catch(() => { console.log('error') });                                
-            });
-        });
-      
-      console.log('Workers Off');
+      bootstrap = './dist/ng2-universal-bootstrap/worker/bootstrap';      
+      libs = ['/node_modules/angular2/bundles/web_worker/ui.dev.js'];
+    } else {
+      bootstrap = './dist/ng2-universal-bootstrap/main/bootstrap';
+      libs = [
+        '/node_modules/angular2/bundles/angular2.dev.js',
+        '/node_modules/rxjs/bundles/Rx.js'
+      ];      
     }
+    
+    Promise.all(libs.map((src) => {
+      return System.import(src);
+    })).then(() => {
+      System.import(bootstrap)
+        .catch(() => console.log('error'));
+    });
   };
   
   return boot;
