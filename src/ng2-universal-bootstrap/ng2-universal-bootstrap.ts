454fe5541b6eba7c30925d67edda743ded86979a
hAWKdv <hawk.dvp@gmail.com>
2016-05-01 01:12:23 +0300

    Feat(Boot function): Add 'libs' configuration option
    An array of additional libraries loaded before the boot.

--- src/ng2-universal-bootstrap/ng2-universal-bootstrap.ts
+++ src/ng2-universal-bootstrap/ng2-universal-bootstrap.ts
@@ -1,41 +1,40 @@
 var ng2UniversalBootstrap = ng2UniversalBootstrap || (function () {
   'use strict';
-  
-  let getWebWorkerConfig = (config) => {
+
+  const _getWebWorkerConfig = (config) => {
     if (typeof config.webworker === 'undefined') {
       return !!window.Worker;
     }
-    
+
     return config.webworker;
   };
-  
-  let boot = (config) => {
+
+  const boot = (config) => {
     config = config || {};
-    config.webworker = getWebWorkerConfig(config);
-    
+    config.webworker = _getWebWorkerConfig(config);
+
     let bootstrap: string;
     let libs: string[];
-        
+
     if (config.webworker) {
-      bootstrap = './dist/ng2-universal-bootstrap/worker/bootstrap';      
+      bootstrap = './dist/ng2-universal-bootstrap/worker/bootstrap';
       libs = ['/node_modules/angular2/bundles/web_worker/ui.dev.js'];
     } else {
       bootstrap = './dist/ng2-universal-bootstrap/main/bootstrap';
       libs = [
         '/node_modules/angular2/bundles/angular2.dev.js',
         '/node_modules/rxjs/bundles/Rx.js'
-      ];      
+      ];
     }
-    
-    // libs = config.coreLibs || libs;
-    // libs = libs.concat(config.libs || []);
-    
+
+    libs = libs.concat(config.libs || []);
+
     Promise.all(libs.map(src => System.import(src)))
       .then(() => {
         System.import(bootstrap)
           .catch(err => console.error('Error:', err));
       });
   };
-  
+
   return boot;
 }());
--- src/ng2-universal-bootstrap/worker/worker.ts
+++ src/ng2-universal-bootstrap/worker/worker.ts
@@ -3,12 +3,11 @@ importScripts('/node_modules/systemjs/dist/system.src.js');
 // Configure SystemJs for web workers
 System.config({ defaultJSExtensions: true });
 
-// import asdsa
 importScripts(
-  '/node_modules/angular2/bundles/web_worker/worker.dev.js',  
-  '/node_modules/angular2/bundles/angular2-polyfills.js'  
+  '/node_modules/angular2/bundles/web_worker/worker.dev.js',
+  '/node_modules/angular2/bundles/angular2-polyfills.js'
 );
 
-// Import the bootstrapping service 
+// Import the bootstrapping service
 System.import('./boot-wrapper')
   .catch(err => console.error('Error:', err));
